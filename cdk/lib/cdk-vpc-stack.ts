75d8f236c9297bf866559bc2e63f1aa809420890
keigo yamamoto <k5.trismegistus@gmail.com>
2024-10-12 01:32:44 +0900

    Create cluster

--- cdk/lib/cdk-vpc-stack.ts
+++ cdk/lib/cdk-vpc-stack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as autoscaling from "aws-cdk-lib/aws-autoscaling";
 import { Construct } from "constructs";
 
 export class CdkVpcStack extends Stack {
@@ -8,20 +11,65 @@ export class CdkVpcStack extends Stack {
 
     const vpc = new ec2.Vpc(this, "EcsHandsOnVpc", {
       vpcName: "EcsHandsOnVpc",
-      subnetConfiguration: [],
       ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
     });
 
-    const publicSubnetOne = new ec2.PublicSubnet(this, "PublicSubnetOne", {
-      availabilityZone: "us-west-2a",
-      vpcId: vpc.vpcId,
-      cidrBlock: "10.0.0.0/20",
+    const ecsSecurityGroup = new ec2.SecurityGroup(this, "EcsSecurityGroup", {
+      vpc,
+      allowAllOutbound: true,
     });
 
-    const publicSubnetTwo = new ec2.PublicSubnet(this, "PublicSubnetTwo", {
-      availabilityZone: "us-west-2b",
-      vpcId: vpc.vpcId,
-      cidrBlock: "10.0.16.0/20",
+    // 必要なポートを開放（例としてHTTPポート80を開放）
+    ecsSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      "Allow HTTP traffic"
+    );
+
+    const clusterRole = new iam.Role(this, "MyRole", {
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+      description: "My EC2 role",
     });
+
+    const cluster = new ecs.Cluster(this, "EcsHandsOnCluster", {
+      vpc: vpc,
+      clusterName: "EcsHandsOnCluster",
+      enableFargateCapacityProviders: true,
+    });
+
+    const autoScalingGroup = new autoscaling.AutoScalingGroup(this, "EcsASG", {
+      vpc,
+      instanceType: new ec2.InstanceType("t3.medium"), // 必要に応じてインスタンスタイプを選択
+      machineImage: ecs.EcsOptimizedImage.amazonLinux2(), // ECS最適化AMI
+      minCapacity: 1,
+      maxCapacity: 1,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC, // AutoScalingGroupを配置するサブネットのタイプ
+      },
+      securityGroup: ecsSecurityGroup,
+      role: clusterRole,
+    });
+
+    const capacityProvider = new ecs.AsgCapacityProvider(
+      this,
+      "EcsAsgCapacityProvider",
+      {
+        autoScalingGroup,
+      }
+    );
+
+    cluster.addAsgCapacityProvider(capacityProvider);
   }
 }
